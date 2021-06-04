1d9a07dcd3383e42b9d33540ae82108c78251f1c
Denise Mignoli <denisemignoli@gmail.com>
2021-06-04 17:20:30 -0300

    Inserção dos exercicios de laço de repetição utilizando Java

---
+++ Bloco I - Scrum, Lógica e Java/2. Java/Logica/Logica_Basico/src/repeticao/Exercicio1DoWhile.java
@@ -0,0 +1,35 @@
+package repeticao;
+
+import java.util.Scanner;
+
+public class Exercicio1DoWhile {
+	/* ENUNCIADO
+	 * Crie um programa que leia um número do teclado até que encontre um número igual a zero. 
+	 * No final, mostre a soma dos números digitados.(DO...WHILE)
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+		
+		int num, soma=0;
+		
+		System.out.println("\nEntre com um número: ");
+		num = leia.nextInt();
+		
+		do
+		{
+			if(num!=0)
+			{
+				soma = soma + num;
+			}
+			System.out.println("\nEntre com um número: ");
+			num = leia.nextInt();
+		}
+		while(num!=0);
+		
+		System.out.println("\nA soma dos números digitados é: "+soma);
+	}
+}
+
