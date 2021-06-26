98da9fd9a447c03dea757049955026119b8a4ff7
Denise Mignoli <denisemignoli@gmail.com>
2021-06-26 11:44:57 -0300

    Atualização

--- Bloco II - MySQL, SpringBoot, Docker part1/2. Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Bloco II - MySQL, SpringBoot, Docker part1/2. Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,8 +1,14 @@
 package org.generation.blogPessoal.controller;
 
 import org.generation.blogPessoal.repository.PostagemRepository;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,12 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll()
+	{
+		return ResponseEntity.ok(repository.findAll());
+	}
 	
 }
