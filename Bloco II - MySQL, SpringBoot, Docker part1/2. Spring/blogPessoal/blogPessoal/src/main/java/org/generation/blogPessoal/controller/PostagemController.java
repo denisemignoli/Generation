dcc91db89a7b2d58f84ad451c369235704a293fe
Denise Mignoli <denisemignoli@gmail.com>
2021-06-27 23:50:50 -0300

    Atualização - findByTitulo e Put

--- Bloco II - MySQL, SpringBoot, Docker part1/2. Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Bloco II - MySQL, SpringBoot, Docker part1/2. Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,10 +42,22 @@ public class PostagemController {
 				.orElse(ResponseEntity.notFound().build()); // função enxuta como se fosse um if else
 	}
 	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo)
+	{
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
 	@PostMapping
-	public ResponseEntity<Postagem> Post(@RequestBody Postagem postagem) // Postagem = Entity
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) // Postagem = Entity
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) // Postagem = Entity
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 		
 	@DeleteMapping("/{id}")
