c60eb2c55ae757676cdeddc64ff4c3991e5440a9
Denise Mignoli <denisemignoli@gmail.com>
2021-07-04 15:24:36 -0300

    Relação ManyToOne - OneToMany

--- Bloco II - MySQL, SpringBoot, Docker part1/2. Spring/Farmacia/src/main/java/Farmacia/farmacia/model/Produto.java
+++ Bloco II - MySQL, SpringBoot, Docker part1/2. Spring/Farmacia/src/main/java/Farmacia/farmacia/model/Produto.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_produto")
@@ -15,22 +18,26 @@ public class Produto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long idProduto;
 
-	@NotEmpty(message = "Produto não pode estar vazio")
+	@NotEmpty(message = "Nome não pode estar vazio")
 	@Size(min = 5, max = 35)
 	private String nomeProduto;
 
-	@NotNull(message = "Produto não pode estar vazia")
+	@NotNull(message = "Descrição não pode estar vazia")
 	@Size(min = 5, max = 255)
 	private String descricaoProduto;
 
-	public Long getId() {
-		return id;
+	@ManyToOne
+	@JsonIgnoreProperties({ "listaDeProdutos", "id" })
+	private Categoria categoria;
+
+	public Long getIdProduto() {
+		return idProduto;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
 	}
 
 	public String getNomeProduto() {
@@ -47,6 +54,14 @@ public class Produto {
 
 	public void setDescricaoProduto(String descricaoProduto) {
 		this.descricaoProduto = descricaoProduto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 }
